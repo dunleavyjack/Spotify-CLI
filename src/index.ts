c5a756922a7841df3ac372788b122e8792f66c07
dunleavyjack <dunleavyjack@gmail.com>
2022-11-26 18:59:29 -0500

    feat(playback): added shuffle toggle

--- src/index.ts
+++ src/index.ts
@@ -3,39 +3,63 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 
 import { Command } from 'commander';
-import { back, login, next, now, pause, play } from './commands/commands';
+import {
+    back,
+    login,
+    next,
+    now,
+    pause,
+    play,
+    intro,
+    shuffleOff,
+    shuffleOn,
+} from './commands/commands';
 
 const program = new Command();
 
 program
     .version('1.0.0')
-    .description('A tool for controlling Spotify from the command line.');
+    .description('A tool for controlling Spotify from the command line.')
+    .action(intro);
 
-program.command('login').description('Login with Spotify OAuth2').action(login);
+program
+    .command('login')
+    .description('Login with Spotify OAuth2.')
+    .action(login);
 
 program
     .command('now')
-    .description('Get the currently playing song')
+    .description('Get the currently playing song.')
     .action(now);
 
 program
     .command('play')
-    .description('Resume playback on current song')
+    .description('Resume playback on current song.')
     .action(play);
 
 program
     .command('pause')
-    .description('Pause the currently playing song')
+    .description('Pause the currently playing song.')
     .action(pause);
 
 program
     .command('next')
-    .description('Skip to next available song in the queue')
+    .description('Skip to next available song in the queue.')
     .action(next);
 
 program
     .command('back')
-    .description('Replay current song or go back')
+    .description('Replay current song or go back.')
     .action(back);
+
+program
+    .command('shuffle-on')
+    .description('Toggle shuffle on. If not on already.')
+    .action(shuffleOn);
+
+program
+    .command('shuffle-off')
+    .description('Toggle shuffle off. If not off already.')
+    .action(shuffleOff);
 
 program.parse(process.argv);
