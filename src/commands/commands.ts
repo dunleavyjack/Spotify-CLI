5c9a9c615cfcd0a437705a18689e8c11bc4942b0
dunleavyjack <dunleavyjack@gmail.com>
2022-11-26 13:20:42 -0500

    feat(playback): added play, pause, next, and back functionality

--- src/commands/commands.ts
+++ src/commands/commands.ts
@@ -1,30 +1,33 @@
+import Conf from 'conf/dist/source';
 import axios, { AxiosResponse } from 'axios';
 import open from 'open';
 import express from 'express';
 import chalk from 'chalk';
-import Configstore from 'configstore';
 import { IncomingMessage, Server, ServerResponse } from 'http';
 import {
     spotifyAuthURL,
-    spotifyHeader,
-    spotifyHeaderTwo,
+    spotifyGetHeader,
+    spotifyPlaybackControlHeader,
+    spotifyPostHeader,
 } from '../api/spotifyUtils';
+import { AUTHORIZATION_CODE } from '../constants';
 
-const config = new Configstore('spotify-cli');
-const { SPOTIFY_BASE_URL, REDIRECT_URL, PORT } = process.env;
+const { SPOTIFY_BASE_URL, SPOTIFY_REDIRECT_URL, PORT } = process.env;
+const config = new Conf();
 
 // Login
 export const login = async () => {
     const app = express();
 
     let resolve: any;
-    const authCodePromise: Promise<string> = new Promise((_resolve) => {
-        resolve = _resolve;
-    });
+    const authorizationCodePromise: Promise<string> = new Promise(
+        (_resolve) => {
+            resolve = _resolve;
+        }
+    );
 
     app.get('/redirect', (req, res) => {
         resolve(req.query.code);
-        // Close window/tab and server
         res.send('<script>window.close();</script > ');
         res.end('');
     });
@@ -33,23 +36,24 @@ export const login = async () => {
         app.listen(PORT);
     open(spotifyAuthURL);
 
-    const code: string = await authCodePromise;
+    const authorizationCode: string = await authorizationCodePromise;
 
     const res = await axios({
         method: 'post',
         url: 'https://accounts.spotify.com/api/token',
         data: {
-            grant_type: 'authorization_code',
-            code,
-            redirect_uri: REDIRECT_URL,
+            grant_type: AUTHORIZATION_CODE,
+            code: authorizationCode,
+            redirect_uri: SPOTIFY_REDIRECT_URL,
         },
-        headers: spotifyHeaderTwo,
+        headers: spotifyPostHeader,
     });
 
-    const token = res.data['access_token'];
+    const accessToken = res.data['access_token'];
+    config.set('accessToken', accessToken);
     server.close();
 
-    console.log('Logged in successfully with token ' + token);
+    console.log('Logged in successfully with token ' + accessToken);
     process.exit(0);
 };
 
@@ -59,29 +63,60 @@ export const now = async (): Promise<void> => {
         const response: AxiosResponse =
             await axios.get<SpotifyApi.CurrentPlaybackResponse>(
                 `${SPOTIFY_BASE_URL}/currently-playing`,
-                spotifyHeader
+                spotifyGetHeader
             );
         const {
             data: { item: currentSong },
         } = response;
         const { album, artists, name } = currentSong;
 
-        console.log(chalk.green(name));
+        console.log('Song: ' + chalk.green(name));
         console.log(
-            chalk.green.dim(
-                artists.map((_artist: any) => _artist.name).join(', ')
-            )
+            'Artist: ' +
+                chalk.green.dim(
+                    artists.map((_artist: any) => _artist.name).join(', ')
+                )
         );
-        console.log(chalk.green.dim(album.name));
+        console.log('Album: ' + chalk.green.dim(album.name));
     } catch (error) {
         console.log('auth error');
+    }
+};
+
+// Pause the current song
+export const pause = async () => {
+    try {
+        axios.put(
+            `${SPOTIFY_BASE_URL}/pause`,
+            null,
+            spotifyPlaybackControlHeader
+        );
+    } catch (error) {
+        console.log('error');
+    }
+};
+
+// Resume playback on current song
+export const play = async () => {
+    try {
+        axios.put(
+            `${SPOTIFY_BASE_URL}/play`,
+            null,
+            spotifyPlaybackControlHeader
+        );
+    } catch (error) {
+        console.log('error');
     }
 };
 
 // Skip to next song
 export const next = async (): Promise<void> => {
     try {
-        axios.post(`${SPOTIFY_BASE_URL}/next`, spotifyHeader);
+        axios.post(
+            `${SPOTIFY_BASE_URL}/next`,
+            null,
+            spotifyPlaybackControlHeader
+        );
     } catch (error) {
         console.log('error');
     }
@@ -90,7 +125,11 @@ export const next = async (): Promise<void> => {
 // Replay current song or go back
 export const back = async (): Promise<void> => {
     try {
-        axios.post(`${SPOTIFY_BASE_URL}/previous`, spotifyHeader);
+        axios.post(
+            `${SPOTIFY_BASE_URL}/previous`,
+            null,
+            spotifyPlaybackControlHeader
+        );
     } catch (error) {
         console.log('error');
     }
---
+++ src/constants.ts
@@ -0,0 +1,2 @@
+export const AUTHORIZATION_CODE = 'authorization_code';
+export const ACCESS_TOKEN = 'accessToken';
