5a8b7e5b50be2f5f99eb7255ba9e44f2a70440a9
dunleavyjack <dunleavyjack@gmail.com>
2022-11-26 04:03:15 -0500

    feat(login): added oauth flow with spotify api

--- src/api/spotifyUtils.ts
+++ src/api/spotifyUtils.ts
@@ -1,8 +1,45 @@
 import { SpotifyHeader } from '../types';
-const authToken = process.env.SAMPLE_AUTH_TOKEN; // Todo: Replace sample auth token with real one
+import { SpotifyAccessToken } from '../types';
+
+const {
+    DEV_AUTH_TOKEN,
+    SPOTIFY_AUTH_URL,
+    SPOTIFY_CLIENT_ID,
+    SPOTIFY_SCOPES,
+    REDIRECT_URL,
+    SPOTIFY_CLIENT_SECRET,
+} = process.env;
 
 export const spotifyHeader: SpotifyHeader = {
     headers: {
-        Authorization: `Bearer ${authToken}`,
+        Authorization: `Bearer ${DEV_AUTH_TOKEN}`,
     },
+};
+
+export const spotifyHeaderTwo: any = {
+    Authorization:
+        'Basic ' +
+        Buffer.from(SPOTIFY_CLIENT_ID + ':' + SPOTIFY_CLIENT_SECRET).toString(
+            'base64'
+        ), // TODO: Remove deprecated Buffer instance
+    'Content-Type': 'application/x-www-form-urlencoded',
+};
+
+export const spotifyAuthURL = `${SPOTIFY_AUTH_URL}?client_id=${SPOTIFY_CLIENT_ID}&scope=${encodeURIComponent(
+    SPOTIFY_SCOPES || ''
+)}&redirect_uri=${encodeURIComponent(
+    REDIRECT_URL || ''
+)}&response_type=code&show_dialog=true`;
+
+export const getSpotifyAccessToken = (
+    spotifyRedirectURL: string
+): SpotifyAccessToken => {
+    return spotifyRedirectURL
+        .slice(1)
+        .split('&')
+        .reduce((prev: any, curr: string) => {
+            const [title, value] = curr.split('=');
+            prev[title] = value;
+            return prev;
+        }, {});
 };
--- src/commands/commands.ts
+++ src/commands/commands.ts
@@ -1,24 +1,64 @@
 import axios, { AxiosResponse } from 'axios';
+import open from 'open';
+import express from 'express';
 import chalk from 'chalk';
-import { spotifyHeader } from '../api/spotifyUtils';
+import Configstore from 'configstore';
+import { IncomingMessage, Server, ServerResponse } from 'http';
+import {
+    spotifyAuthURL,
+    spotifyHeader,
+    spotifyHeaderTwo,
+} from '../api/spotifyUtils';
 
-const { BASE_URL, SAMPLE_AUTH_TOKEN } = process.env;
-
-// Test
-export const test = () => {
-    console.log('Auth below');
-    console.log(SAMPLE_AUTH_TOKEN);
-};
+const config = new Configstore('spotify-cli');
+const { SPOTIFY_BASE_URL, REDIRECT_URL, PORT } = process.env;
 
 // Login
-export const login = () => {};
+export const login = async () => {
+    const app = express();
+
+    let resolve: any;
+    const authCodePromise: Promise<string> = new Promise((_resolve) => {
+        resolve = _resolve;
+    });
+
+    app.get('/redirect', (req, res) => {
+        resolve(req.query.code);
+        // Close window/tab and server
+        res.send('<script>window.close();</script > ');
+        res.end('');
+    });
+
+    const server: Server<typeof IncomingMessage, typeof ServerResponse> =
+        app.listen(PORT);
+    open(spotifyAuthURL);
+
+    const code: string = await authCodePromise;
+
+    const res = await axios({
+        method: 'post',
+        url: 'https://accounts.spotify.com/api/token',
+        data: {
+            grant_type: 'authorization_code',
+            code,
+            redirect_uri: REDIRECT_URL,
+        },
+        headers: spotifyHeaderTwo,
+    });
+
+    const token = res.data['access_token'];
+    server.close();
+
+    console.log('Logged in successfully with token ' + token);
+    process.exit(0);
+};
 
 // Get current song
 export const now = async (): Promise<void> => {
     try {
         const response: AxiosResponse =
             await axios.get<SpotifyApi.CurrentPlaybackResponse>(
-                `${BASE_URL}/currently-playing`,
+                `${SPOTIFY_BASE_URL}/currently-playing`,
                 spotifyHeader
             );
         const {
@@ -41,7 +81,7 @@ export const now = async (): Promise<void> => {
 // Skip to next song
 export const next = async (): Promise<void> => {
     try {
-        axios.post(`${BASE_URL}/next`, spotifyHeader);
+        axios.post(`${SPOTIFY_BASE_URL}/next`, spotifyHeader);
     } catch (error) {
         console.log('error');
     }
@@ -50,7 +90,7 @@ export const next = async (): Promise<void> => {
 // Replay current song or go back
 export const back = async (): Promise<void> => {
     try {
-        axios.post(`${BASE_URL}/previous`, spotifyHeader);
+        axios.post(`${SPOTIFY_BASE_URL}/previous`, spotifyHeader);
     } catch (error) {
         console.log('error');
     }
